fc46faceffa7eefe15715274be425e1bc3791db9
Andrean <shufhadha@gmail.com>
2023-08-14 15:22:21 +0800

    feat(TICKET-0): added store table and related models, factory, seeder etc

---
+++ database/factories/StoreFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\WithFaker;
+
+class StoreFactory extends Factory
+{
+    use WithFaker;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->sentence(3),
+        ];
+    }
+}
